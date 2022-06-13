9e13694dc89259f84d779eb073bcdd6538e75d72
khanh99 <khanh9a1999@gmail.com>
2022-06-14 00:04:56 +0700

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Navigation = () => {
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Product</li>
+      </ul>
+    </nav>
+  )
+}
